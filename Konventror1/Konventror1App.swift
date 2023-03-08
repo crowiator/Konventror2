eaf36fa6591491a117605b2802970d4479629b0f
Samuel Vrana <vrana.samuel@gmail.com>
2023-03-08 08:38:28 +0100

    Initial Commit

---
+++ Konventror1/Konventror1App.swift
@@ -0,0 +1,17 @@
+//
+//  Konventror1App.swift
+//  Konventror1
+//
+//  Created by crow on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Konventror1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
